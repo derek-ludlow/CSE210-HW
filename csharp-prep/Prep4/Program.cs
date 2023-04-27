8452f0b4e6c53056366b30cfe70911c0bfd985f0
Derek Ludlow <lud15002@byui.edu>
2023-04-27 11:34:06 -0600

    Submitting initial completion of csharp prep assignments 3-5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
 
+// LISTS 
+// List<int> numbers = new List<int>();
+// List<string> words = new List<string>();
+// using System.Collections.Generic; - Any file that uses lists(or any other standard collection), must refer to that library at the top of the file
+
+// ADDING ITEMS TO THE LIST 
+// List<string> words = new List<string>();
+// words.Add("phone");
+// words.Add("keyboard");
+// words.Add("mouse");
+
+// GETTING THE LIST SIZE 
+// Console.WriteLine(words.Count);
+
+// ITERATING THROUGH A LIST 
+// foreach (string word in words)
+// {
+//     Console.WriteLine(word);
+// }
+// ACCESSING ITEMS BY THEIR INDEX 
+// for (int i = 0; i < words.Count; i++)
+// {
+//     Console.WriteLine(words[i]);
+// }
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit)");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        // PART 1 - compute the sum 
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // PART 2 - compute average 
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        // PART 3 - find max 
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The mas is: {max}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,87 @@
 using System;
 
+// FUNCTIONS 
+// returnType FunctionName(dataType parameter1, dataType parameter2)
+// {
+//     // function_body
+// }
+// C#
+// void DisplayMessage()
+// {
+//     Console.WriteLine("Hello world!");
+// }
+// Python 
+// def display_message():
+//     print("Hello World")
+
+// SINGLE STRING PARAMETER 
+// void DisplayPersonalMessage(string userName)
+// {
+//     Console.WriteLine($"Hello {userName}");
+// }
+// ACCEPTS TWO INTEGERS AS A PARAMETER
+// int AddNumbers(int first, int second)
+// {
+//     int sum = first + second;
+//     return sum;
+// }
+// FUNCTIONS VS METHODS 
+// static void DisplayMessage()
+// {
+//     Console.WriteLine("Hello world!");
+// }
+
+// static void DisplayPersonalMessage(string userName)
+// {
+//     Console.WriteLine($"Hello {userName}");
+// }
+
+// static int AddNumbers(int first, int second)
+// {
+//     int sum = first + second;
+//     return sum;
+// }
+// ***** USE STATIC FOR ALL YOUR FUNCTIONS UNTIL WE START WRITING CLASSES 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
     }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
 }
