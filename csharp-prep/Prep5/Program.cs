263fa3afeeab6bf78c9324cab416cfa04ae3a60d
Derek Ludlow <lud15002@byui.edu>
2023-04-27 14:09:34 -0600

    Adding spacing and fixing a line typo in csharp Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -62,7 +62,7 @@ class Program
     }
     static string PromptUserName()
     {
-        Console.WriteLine("Please enter your name: ");
+        Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
 
         return name;
