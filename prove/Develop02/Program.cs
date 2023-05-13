490726b3d01b790755601537946ff1cdcfeac5f4
Derek Ludlow <lud15002@byui.edu>
2023-05-13 13:27:43 -0600

    Pushing week 4 prove assignment-journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,176 @@
+
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = PromptGenerator.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    Console.Write("Response: ");
+                    string response = Console.ReadLine();
+                    journal.AddEntry(prompt, response);
+                    break;
+
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to save to: ");
+                    string saveFileName = Console.ReadLine();
+                    SaveAndLoad.SaveToFile(journal, saveFileName);
+                    break;
+
+                case "4":
+                    Console.Write("Enter filename to load from: ");
+                    string loadFileName = Console.ReadLine();
+                    SaveAndLoad.LoadFromFile(journal, loadFileName);
+                    break;
+
+                case "5":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Goodbye!");
+    }
+}
+
+class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public DateTime Date { get; set; }
+
+    public Entry(string prompt, string response, DateTime date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+    public override string ToString()
+    {
+        return $"[{Date.ToShortDateString()}] {Prompt}: {Response}";
+    }
+}
+
+class Journal
+{
+    private List<Entry> entries;
+
+    public Journal()
+    {
+        entries = new List<Entry>();
+    }
+
+    public void AddEntry(string prompt, string response)
+    {
+        entries.Add(new Entry(prompt, response, DateTime.Now));
+    }
+
+    public void DisplayEntries()
+    {
+        if (entries.Count == 0)
+        {
+            Console.WriteLine("No entries found.");
+        }
+        else
+        {
+            foreach (Entry entry in entries)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+
+    public void SaveToFile(string fileName)
+    {
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry entry in entries)
+            {
+                outputFile.WriteLine($"{entry.Date};{entry.Prompt};{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string fileName)
+    {
+        entries.Clear();
+        using (StreamReader sr = new StreamReader(fileName))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] parts = line.Split(';');
+                DateTime date = DateTime.Parse(parts[0]);
+                string prompt = parts[1];
+                string response = parts[2];
+                entries.Add(new Entry(prompt, response, date));
+            }
+        }
+    }
+}
+
+class SaveAndLoad
+{
+    public static void SaveToFile(Journal journal, string fileName)
+    {
+        journal.SaveToFile(fileName);
+        Console.WriteLine($"Journal saved to file: {fileName}");
+    }
+
+    public static void LoadFromFile(Journal journal, string fileName)
+    {
+        journal.LoadFromFile(fileName);
+        Console.WriteLine($"Journal loaded from file: {fileName}");
+    }
+}
+
+class PromptGenerator
+{
+    private static List<string> prompts = new List<string>()
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What is something I learned today?",
+        "What made me laugh today?",
+        "What was something new I tried today?",
+        "What happened today that you are most gratefu for?"
+    };
+
+    public static string GetRandomPrompt()
+    {
+        return prompts[new Random().Next(prompts.Count)];
     }
 }
