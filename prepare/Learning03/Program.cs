35f2f5e38a8cf1da6907655aec369db00cebbb16
Derek Ludlow <lud15002@byui.edu>
2023-05-16 15:47:38 -0600

    Learning03 Activity completion

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,103 @@
 using System;
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom;
+    }
+
+    public int GetNumerator()
+    {
+        return numerator;
+    }
+
+    public void SetNumerator(int top)
+    {
+        numerator = top;
+    }
+
+    public int GetDenominator()
+    {
+        return denominator;
+    }
+
+    public void SetDenominator(int bottom)
+    {
+        denominator = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString()); // Output: 1/1
+        Console.WriteLine(fraction1.GetDecimalValue()); // Output: 1
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString()); // Output: 5/1
+        Console.WriteLine(fraction2.GetDecimalValue()); // Output: 5
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString()); // Output: 3/4
+        Console.WriteLine(fraction3.GetDecimalValue()); // Output: 0.75
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString()); // Output: 1/3
+        Console.WriteLine(fraction4.GetDecimalValue()); // Output: 0.3333333333333333
     }
 }
+//         var savings = new Account();
+//         savings.Deposit(500);
+//         savings.Deposit(1000);
+
+//         // savings._transactions.Add(400);
+//     }
+// }
+
+// public class Account
+// {
+//     private List<int> _transactions = new List<int>();
+
+//     public void Deposit(int amount)
+//     {
+//         _transactions.Add(amount);
+
+//     public int Balance()
+//     {
+//         var balance = 0;
+//         foreach (var transaction in _transactions)
+//         {
+//             balance += transaction;
+//         }
+//         return balance;
+//     }
+// }
+
