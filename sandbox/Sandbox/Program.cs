42d7e73f664527921c4d37cf90b671caae3c6f2f
Derek Ludlow <lud15002@byui.edu>
2023-05-31 21:02:44 -0600

    Pushing Sandbox program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+
+        var personOne = new Person("luke skywalker");
+
+        var byuiPerson = new BYUIPerson("obi-wan", "1");
+
+        var studentOne = new Student("darth vader", "2", "the dark side of the force");
+
+
     }
+}
+// Parent class 
+// Super class 
+// Base class 
+class Person
+{
+    protected string _name;
+
+    private string _height;
+
+    public Person(string name)
+    {
+        _name = name;
+    }
+
+    public Person(string name, string height)
+    {
+
+    }
+}
+class BYUIPerson : Person
+{
+    protected string _iNumber;
+
+    public BYUIPerson(string name, string iNumber) : base(name)
+    {
+        _iNumber = iNumber;
+    }
+}
+// Sub class 
+// Child class 
+class Student : BYUIPerson
+{
+    private string _major;
+
+    public Student(string name, string iNumber, string major) : base(name, iNumber)
+    {
+        _major = major;
+    }
+}
+
+class Teacher : BYUIPerson
+{
+    private string _department;
+
+    public Teacher(string name, string iNumber, string department) : base(name, iNumber)
+    {
+        _department = department;
+    }
+
+
 }
